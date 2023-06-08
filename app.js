1113973780e8a90941946865eaa785172478348c
chachagof <chacha0932@gmail.com>
2023-06-08 16:53:33 +0800

    feat: add main page

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
@@ -22,7 +22,7 @@ app.engine('handlebars',exphbs.engine({defaultLayout:'main'}))
 app.set('view engine','handlebars')
 
 app.get('/',(req,res)=>{
-  res.render('index')
+  res.render('schedule')
 })
 
 app.listen(port,()=>{
