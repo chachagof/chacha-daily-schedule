f512237dd8ec475002b9fba1b05dfb7b4da1d7a0
chachagof <chacha0932@gmail.com>
2023-06-14 16:16:28 +0800

    feat:add progess line and progress function

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,3 +1,4 @@
+//倒數計時器
 function countDown(){
   setInterval(()=>{
   document.getElementById('hours').innerHTML = `${dayjs.duration(dayjs().endOf('day').diff(dayjs())).hours()}`
@@ -6,5 +7,20 @@ function countDown(){
 },1000)
 }
 
+//進度條
+function taskProgress (){
+  const tasks = document.querySelectorAll('.bi')
+  const finishTasks = document.querySelectorAll('.bi-clipboard-check')
+  const progress = document.getElementById('progress')
+  const taskCountWord = document.querySelector('.task-count-word')
+  setInterval(()=>{
+    progressRate = Math.floor((finishTasks.length/tasks.length) * 100)
+    taskCountWord.innerHTML = `完成進度:${progressRate}%`
+    progress.style.width = `${progressRate}%`
+  },1000)
+}
+
+
 
 countDown()
+taskProgress()
