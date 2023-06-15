1a497a5ae21bc1ffde335337c151c187aa127897
chachagof <chacha0932@gmail.com>
2023-06-15 18:06:18 +0800

    feat:add user login page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const task = require('./modules/task')
+const user = require('./modules/user')
 
 router.use('/task',task)
+router.use('/user',user)
 router.use('/',home)
 
 module.exports = router
