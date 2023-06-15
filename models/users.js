cfa8ded297af699bf91e8c3c9abcc677b8b9e5e2
chachagof <chacha0932@gmail.com>
2023-06-15 17:50:37 +0800

    feat:create users model

---
+++ models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+  userName:{
+    type:String,
+    require:true
+  },
+  email:{
+    type:String,
+    require:true
+  },
+  password:{
+    type:String,
+    require:true
+  },
+  createAt:{
+    type: Date,
+    default: Date.now
+  }
+})
